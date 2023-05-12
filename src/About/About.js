2e1a7ae0b5c01d4ff928febc1cbf6d81db08822e
sandeepsharma2002 <sandeepsharmajec24@gmail.com>
2023-05-12 18:49:24 +0530

    my Photo

--- src/About/About.js
+++ src/About/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import photo from "../assets/my-photo.jpg"
+import photo from "../assets/my-photo.png"
 import './About.css'
 export default function About() {
   return (
